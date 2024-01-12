813cd80b566a7ec539b964c755dd22b90b94d704
Luka <luka.gjuric@spandevacademy.onmicrosoft.com>
2024-01-12 17:07:52 +0100

    static css added, not loaded on page properly

--- src/main/java/project/config/WebSecurityConfig.java
+++ src/main/java/project/config/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests()
-			.antMatchers( "/","/login","/spotify",  "/song-mood-detection", "/logout", "/oauth/**").permitAll()
+			.antMatchers( "/","/login","/spotify",  "/song-mood-detection", "/logout", "/oauth/**", "/resources/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.oauth2Login()
